950298c9917874d807a7053d126bbdf7de3aa82d
markozuzic <markozuzic@nordnet.rs>
2018-07-10 00:23:51 +0200

    Add files via upload

---
+++ chatbot-client/index.js
@@ -0,0 +1,96 @@
+var me = {};
+me.avatar = "darth2.jpg";
+
+var you = {};
+you.avatar = "yoda.png";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time){
+    if (time === undefined){
+        time = 0;
+    }
+    var control = "";
+    var date = formatAMPM(new Date());
+
+    if (who == "me"){
+        control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                            '<div class="text text-l">' +
+                                '<p>'+ text +'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';
+    }else{
+        control = '<li style="width:100%;">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="text text-r">' +
+                                '<p>'+text+'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +
+                  '</li>';
+    }
+    setTimeout(
+        function(){
+            $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
+        }, time);
+
+}
+
+function resetChat(){
+    $("ul").empty();
+}
+
+function keyPressedFunction(e) {
+     if (e.which == 13){
+        var text = $("#chatField").val();
+        if (text !== ""){
+            insertChat("me", text);
+            $("#chatField").val('');
+            sendRequest(text)
+        }
+    }
+}
+
+function sendRequest(message) {
+    $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:5000/chat",
+    contentType: "text/plain",
+    data: message,
+    success: function(response){insertChat("Yoda", response)}
+});
+
+}
+
+$(".mytext").on("keydown", function(e){
+    alert("trigerujeeee")
+    if (e.which == 13){
+        var text = $(this).val();
+        if (text !== ""){
+            insertChat("me", text);
+            $(this).val('');
+        }
+    }
+});
+
+$('body > div > div > div:nth-child(2) > span').click(function(){
+    $(".mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
+})
+
+//-- Clear Chat
+resetChat();
+
